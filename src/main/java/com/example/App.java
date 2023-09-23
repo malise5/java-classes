f435af99c6789406c6f2ebfe5dcf3e7924e49949
Halkano Malise <kudezx@gmail.com>
2023-09-23 19:14:33 +0300

    test class

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,4 +2,12 @@ package com.example;
 
 public class App {
 
+    String greetings() {
+        return "Hello World";
+    }
+
+    int sum(int a, int b) {
+        return a + b;
+    }
+
 }
--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,12 +1,27 @@
 package com.example;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
 public class AppTest {
+    App kudez = new App();
+
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    public void shouldReturnHelloWorld() {
+
+        String expected = "Hello World";
+        String result = kudez.greetings();
+
+        assertEquals(expected, result);
     }
+
+    @Test
+    public void sum() {
+        int expected = 3;
+        int result = kudez.sum(1, 2);
+
+        assertEquals(expected, result);
+    }
+
 }
