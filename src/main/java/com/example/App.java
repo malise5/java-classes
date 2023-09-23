97896b121fab4b79f49a66860586aea8ac3aa122
Halkano Malise <kudezx@gmail.com>
2023-09-23 20:23:07 +0300

    updated junit

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,12 +2,32 @@ package com.example;
 
 public class App {
 
-    String greetings() {
+    private String personName;
+
+    private static int numbers;
+
+    public App(String personName) {
+        this.personName = personName;
+    }
+
+    public App() {
+        App.numbers++;
+    }
+
+    public String hello(String name) {
+        return "Hello " + name;
+    }
+
+    public String greetings() {
         return "Hello World";
     }
 
-    int sum(int a, int b) {
+    public int sum(int a, int b) {
         return a + b;
+    }
+
+    public static int numberOfPersons() {
+        return numbers;
     }
 
 }
--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -5,11 +5,10 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class AppTest {
-    App kudez = new App();
 
     @Test
     public void shouldReturnHelloWorld() {
-
+        App kudez = new App();
         String expected = "Hello World";
         String result = kudez.greetings();
 
@@ -18,10 +17,31 @@ public class AppTest {
 
     @Test
     public void sum() {
+        App kudez = new App();
         int expected = 3;
         int result = kudez.sum(1, 2);
 
         assertEquals(expected, result);
+    }
+
+    @Test
+    public void hello() {
+        App app = new App("Kudez");
+
+        String expected = "Hello Kudez";
+        String actual = app.hello("Kudez");
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void returnNumberOfPersons() {
+        App person = new App();
+        App person2 = new App();
+
+        assertEquals(2, App.numberOfPersons());
+
     }
 
 }
