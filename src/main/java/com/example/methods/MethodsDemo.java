24a9e1610479382d39aed9f411468ec0d98de469
Halkano Malise <kudezx@gmail.com>
2023-09-23 16:28:23 +0300

    WHat is  Method

---
+++ src/main/java/com/example/methods/MethodsDemo.java
@@ -0,0 +1,13 @@
+package com.example.methods;
+
+public class MethodsDemo {
+
+  static void hellow(String string) {
+    System.out.println("Hello " + string);
+  }
+
+  public static void main(String[] args) {
+    MethodsDemo.hellow("KUdez");
+  }
+
+}
