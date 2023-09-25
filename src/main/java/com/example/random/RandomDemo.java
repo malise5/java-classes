8db8514faab6de765e88735bc86b0f62d8b13147
Halkano Malise <kudezx@gmail.com>
2023-09-26 00:32:39 +0300

    update

--- src/main/java/com/example/random/RandomDemo.java
+++ src/main/java/com/example/random/RandomDemo.java
@@ -1,9 +1,27 @@
 package com.example.random;
 
+import java.io.IOException;
 import java.util.Random;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+
+import com.example.logger.CustomFormatter;
+import com.example.logger.FileLogging;
 
 public class RandomDemo {
-  public static void main(String[] args) {
+  private static final Logger LOGGER = Logger.getLogger(FileLogging.class.getName());
+
+  public static void main(String[] args) throws SecurityException, IOException {
+    FileHandler fh = new FileHandler("log.txt");
+    CustomFormatter formatter = new CustomFormatter();
+
+    LOGGER.addHandler(fh);
+    fh.setFormatter(formatter);
+
+    LOGGER.info("info Message Logging\n");
+    LOGGER.severe("Error Message\n");
+    LOGGER.warning("Warning Message Logn\n");
+
     Random number = new Random();
 
     int x = number.nextInt(10);
@@ -12,5 +30,6 @@ public class RandomDemo {
     System.out.println(x);
     System.out.println(y * 10);
 
+    LOGGER.info("rANDOMdEMO");
   }
 }
