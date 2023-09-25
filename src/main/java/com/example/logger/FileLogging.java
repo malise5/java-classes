cb9ba710ca238e2584808c4baea9c98adbf3ffa3
Halkano Malise <kudezx@gmail.com>
2023-09-25 14:58:07 +0300

    ufixed a bug

--- src/main/java/com/example/logger/FileLogging.java
+++ src/main/java/com/example/logger/FileLogging.java
@@ -1,14 +1,7 @@
 package com.example.logger;
 
 import java.io.IOException;
-// import java.time.Instant;
-// import java.time.LocalDateTime;
-// import java.time.ZoneId;
-// import java.time.format.DateTimeFormatter;
 import java.util.logging.*;
-// import java.util.logging.Formatter;
-// import java.util.logging.LogRecord;
-// import java.util.logging.Logger;
 
 public class FileLogging {
   private static final Logger LOGGER = Logger.getLogger(FileLogging.class.getName());
@@ -32,24 +25,5 @@ public class FileLogging {
     LOGGER.severe("Error Message\n");
     LOGGER.warning("Warning Message Logn\n");
   }
-
-  // static class CustomFormatter extends Formatter {
-
-  // @Override
-  // public String format(LogRecord record) {
-
-  // DateTimeFormatter pattern = DateTimeFormatter.ofPattern("yyy-MM-dd
-  // HH:mm:ss");
-  // String method = record.getSourceMethodName();
-  // String level = record.getLevel().getName();
-  // String message = record.getMessage();
-  // // record.getParameters();
-  // Instant instant = record.getInstant();
-  // LocalDateTime now = LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
-  // return pattern.format(now) + " || " + "instant" + " || " + method + " || " +
-  // level + " || " + message;
-  // }
-
-  // }
 
 }
